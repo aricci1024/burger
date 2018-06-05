677fbfe7ceccf72e491c67359b4c4f7059127594
aricci <alexander.p.ricci@gmail.com>
2018-06-05 11:47:33 -0700

    files created; orm and controller started

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Big Mac');
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger with grilled onions');

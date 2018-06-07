f62444999f07717e1adb81dd7f86fad265820597
aricci <alexander.p.ricci@gmail.com>
2018-06-07 12:52:54 -0700

    need to get button working

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -13,12 +13,11 @@ router.get("/", function(req, res) {
   
 router.post("/api/burgers", function(req, res) {
     burger.insertOne([
-        "burger_name"
+        "burger_name", "devoured"
     ], [
-        req.body.burger_name
+        req.body.burger_name, req.body.devoured
     ], function(result) {
-        // Send back the ID of the new quote
-        res.redirect('/')
+        res.json({id: result.insertId});
     });
 });
   
@@ -27,7 +26,12 @@ router.put("/api/burgers/:id", function(req, res) {
     burger.updateOne({
         devoured: true
     }, condition, function(result) {
-        res.redirect('/')
+        if (result.changedRows == 0) {
+            // If no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+          } else {
+            res.status(200).end();
+          }
     });
 });
 

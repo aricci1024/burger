b0e33eff60a606f0c8eb7682182cdf3537223eb6
aricci <alexander.p.ricci@gmail.com>
2018-06-07 13:08:23 -0700

    updated schema

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -11,27 +11,22 @@ router.get("/", function(req, res) {
     });
 });
   
-router.post("/api/burgers", function(req, res) {
+router.post("/", function(req, res) {
     burger.insertOne([
         "burger_name", "devoured"
     ], [
         req.body.burger_name, req.body.devoured
     ], function(result) {
-        res.json({id: result.insertId});
+        res.redirect("/");
     });
 });
   
-router.put("/api/burgers/:id", function(req, res) {
+router.put("/:id", function(req, res) {
     var condition = "id = " + req.params.id;
     burger.updateOne({
-        devoured: true
+        devoured: req.body.devoured
     }, condition, function(result) {
-        if (result.changedRows == 0) {
-            // If no rows were changed, then the ID must not exist, so 404
-            return res.status(404).end();
-          } else {
-            res.status(200).end();
-          }
+        res.redirect("/");
     });
 });
 
